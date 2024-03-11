2f2d7c5747c397e9e26801a63182dd7f7b0f4b89
노은하 <kqlcms256@gmail.com>
2024-03-11 16:31:42 +0900

    2024-03-11 노은하 CancelResasonDTO.java 업로드

---
+++ Woori-man/src/com/project/woori/CancelReasonDTO.java
@@ -0,0 +1,28 @@
+package com.project.woori;
+
+// 2024-03-11 노은하
+// 모임 철회 사유 DTO
+public class CancelReasonDTO
+{
+	private String crc_code;
+	private String crc_reason;
+	
+	// getter / setter 구성
+	public String getCrc_code()
+	{
+		return crc_code;
+	}
+	public void setCrc_code(String crc_code)
+	{
+		this.crc_code = crc_code;
+	}
+	public String getCrc_reason()
+	{
+		return crc_reason;
+	}
+	public void setCrc_reason(String crc_reason)
+	{
+		this.crc_reason = crc_reason;
+	}
+
+}
