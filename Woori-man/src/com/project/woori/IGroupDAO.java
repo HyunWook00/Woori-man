b3a8153e6e3c986324ee1636402e036925ab2f85
김민지 <frozen191121@gmail.com>
2024-03-12 22:20:18 +0900

    2024-03-12 김민지 DTO, Controller commit

---
+++ Woori-man/src/com/project/woori/IGroupDAO.java
@@ -0,0 +1,16 @@
+package com.project.woori;
+
+import java.util.ArrayList;
+
+public interface IGroupDAO
+{
+	public GroupDTO groupPoint (String cg_code);						// 그룹 포인트 조회
+	
+	public ArrayList<BoardDTO> newBorad(String cg_code);				// 자유게시판 조회
+	public ArrayList<HistoryDTO> newHistory(String cg_code);			// 히스토리 조회
+	public ArrayList<CsDTO> noticesList();								// 공지사항 제목 조회
+	
+	public ArrayList<MeetingDTO> impromptuMeeting(String cg_code);		// 현재 참석 여부 확인중인 번개 모임 조회
+	public ArrayList<MeetingDTO> regularMeeting(String cg_code);		// 현재 참석 여부 확인중인 정기 모임 조회
+	
+}
