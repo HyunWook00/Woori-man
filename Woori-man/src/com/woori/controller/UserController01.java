a0abbb8b0cf3f369bd29725a14b19063ebc1b523
박가영 <g.park1995@gmail.com>
2024-03-16 13:35:33 +0900

    2024-03-16 UserController01, UserAccountDAO 박가영

--- Woori-man/src/com/woori/controller/UserController01.java
+++ Woori-man/src/com/woori/controller/UserController01.java
@@ -17,6 +17,8 @@ package com.woori.controller;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,39 +33,44 @@ import com.woori.dto.UserDTO;
 public class UserController01
 {
 	// 나의 플랫폼 계정정보 조회
-	@RequestMapping(value = "/user.woori")
-	public String userList(Model model) throws SQLException, ClassNotFoundException
-	{
-		UserAccountDAO dao = new UserAccountDAO();
-		UserDTO myProfile = new UserDTO();
-		
-		int idx = 0;
-		
-		try
+		@RequestMapping(value = "/user.woori")
+		public String userList(Model model, HttpSession session) throws SQLException, ClassNotFoundException
 		{
-			// String us_Code = "4";
-			myProfile = dao.myProfile();
+			UserAccountDAO dao = new UserAccountDAO();
+			UserDTO myProfile = new UserDTO();
+
+			UserDTO dto = (UserDTO) session.getAttribute("userDTO");
+			String us_code = dto.getUs_code();
 			
-			idx = myProfile.getUs_email().indexOf("@");
+			int idx = 0;
+			
+			try
+			{
+				// String us_Code = "4";
+				myProfile = dao.myProfile(us_code);
+				
+				idx = myProfile.getUs_email().indexOf("@");
+				
 
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+
+			model.addAttribute("myProfile", myProfile);
+			
+			model.addAttribute("email1", myProfile.getUs_email().substring(0, idx));	// 이메일 주소 보내기
+			model.addAttribute("tel2", myProfile.getUs_tel().substring(3, 7));			// 핸드폰 번호 보내기
+			model.addAttribute("tel3", myProfile.getUs_tel().substring(7, 11));
+			model.addAttribute("zipCode", myProfile.getUs_zipcode());
+			model.addAttribute("addr1", myProfile.getUs_addr1());						// 주소 보내기
+			model.addAttribute("addr2", myProfile.getUs_addr2());
+			
+			//System.out.println("컨트롤러 들어옴~~");
+			
+			return "/WEB-INF/view/PlatformMypageMain2.jsp";
+			// return "/WEB-INF/PlatformMypageMain.jsp";
 		}
-
-		model.addAttribute("myProfile", myProfile);
-		
-		model.addAttribute("email1", myProfile.getUs_email().substring(0, idx));	// 이메일 주소 보내기
-		model.addAttribute("tel2", myProfile.getUs_tel().substring(3,7));			// 핸드폰 번호 보내기
-		model.addAttribute("tel3", myProfile.getUs_tel().substring(7, 11));
-		model.addAttribute("zipCode", myProfile.getUs_zipcode());
-		model.addAttribute("addr1", myProfile.getUs_addr1());																			// 주소 보내기
-		model.addAttribute("addr2", myProfile.getUs_addr2());
-		
-		
-		return "/WEB-INF/view/PlatformMypageMain2.jsp";
-		// return "/WEB-INF/PlatformMypageMain.jsp";
-	}
 
 	// ============================================================================
 	// 데이터 입력(계정 수정) 폼 요청 관련 액션 처리 → 폼에 기존 데이터 받아오기
@@ -99,66 +106,63 @@ public class UserController01
 	
 	//-==========================================================
 	
-	// 데이터 입력(계정 수정) 액션 처리
-	
-	@RequestMapping(value = "/platformaccountupdate.woori", method = RequestMethod.POST)
-	// public String accountUpdate(UserDTO dto) 
-	public String accountUpdate(@RequestParam("us_Email1") String us_Email1,
-			@RequestParam("us_Email2") String us_Email2, @RequestParam("tel_First") String tel_First,
-			@RequestParam("us_Tel1") String us_Tel1, @RequestParam("us_Tel2") String us_Tel2,
-			@RequestParam("us_addr1") String us_addr1, @RequestParam("us_addr2") String us_addr2,
-			@RequestParam("us_zipCode") String us_zipCode) throws ClassNotFoundException, SQLException
-
-	{
-		String result = "";
-
-		//int us_code = 3859105;
-
-		String Email = us_Email1 + "@" + us_Email2;
-
-		String Tel = tel_First + us_Tel1 + us_Tel2;
-
-		String zipCode = us_zipCode;
-		
-		String addr1 = us_addr1;
-		
-		String addr2 = us_addr2;
-
-		
-		 //System.out.println(us_Email); System.out.println(us_Tel);
-		 //System.out.println(us_Addr);
-
-		try
+		// 데이터 입력(계정 수정) 액션 처리
+		@RequestMapping(value = "/platformaccountupdate.woori", method = RequestMethod.POST)
+		// public String accountUpdate(UserDTO dto) 
+		public String accountUpdate(HttpSession session , 
+				@RequestParam("us_Email1") String us_Email1,
+				@RequestParam("us_Email2") String us_Email2 , @RequestParam("tel_First") String tel_First,
+				@RequestParam("us_Tel2") String us_Tel2     , @RequestParam("us_Tel3") String us_Tel3,
+				@RequestParam("us_addr1") String us_addr1   , @RequestParam("us_addr2") String us_addr2,
+				@RequestParam("us_zipCode") String us_zipCode) throws ClassNotFoundException, SQLException
 		{
-			UserAccountDAO dao = new UserAccountDAO();
-			UserDTO dto = new UserDTO();
-
-			// 회원프사, 회원전화번호, 회원주소, 회원이메일, 양음력
-			// → ● 3/7 뷰페이지 수정 후 양음력 추가
-			//dto.setUs_profile(us_profile);
-			dto.setUs_tel(Tel);
-			dto.setUs_zipcode(zipCode);
-			dto.setUs_addr1(addr1);
-			dto.setUs_addr2(addr2);
-			dto.setUs_email(Email);
-			//dto.setUs_lunar(lunar);
 			
+			UserDTO sessiondto = (UserDTO) session.getAttribute("userDTO");
+			String us_code = sessiondto.getUs_code();		// 세션으로 us_code 받아오기
+			
+			String result = "";
+			
+			//int us_code = 3859105;
+			
+			String us_profile = "";
+			String Email = us_Email1 + "@" + us_Email2;	 	// 뷰페이지에서 이메일 받아오기		
+			String Tel = tel_First + us_Tel2 + us_Tel3;		// 뷰페이지에서 전화번호 받아오기
+			String zipCode = us_zipCode;					// 뷰페이지에서 주소들 받아오기
+			String addr1 = us_addr1;
+			String addr2 = us_addr2;
+			
+			 //System.out.println(us_Email); System.out.println(us_Tel);
+			 //System.out.println(us_Addr);
+			try
+			{
+				UserAccountDAO dao = new UserAccountDAO();
+				UserDTO dto = new UserDTO();
 
-			// dao.connection();
+				// 회원프사, 회원전화번호, 회원주소, 회원이메일
+				dto.setUs_profile(us_profile);
+				dto.setUs_tel(Tel);
+				dto.setUs_zipcode(zipCode);
+				dto.setUs_addr1(addr1);
+				dto.setUs_addr2(addr2);
+				dto.setUs_email(Email);
+				dto.setUs_code(us_code);
+				//dto.setUs_lunar(lunar);
+				
+				// dao.connection();
 
-			dao.modify(dto);
+				dao.modify(dto);
 
-			// dao.close();
+				// dao.close();
 
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+
+			result = "redirect:user.woori";
+			return result;
 		}
-
-		result = "redirect:user.woori";
-		return result;
-	}
-	
+		
 	
 	// ====================================================================================
 	// 플랫폼 비밀번호 수정 관련 액션 처리
@@ -173,98 +177,113 @@ public class UserController01
 	}
 	*/
 	
-	// 플랫폼 비밀번호 수정
-	@RequestMapping(value = "/platformpwdupdate.woori", method = RequestMethod.POST)
-	public String pwdUpdate(@RequestParam("now_pwd") String now_pwd, @RequestParam("new_pwd1") String new_pwd1)
-	{
-		//System.out.println("컨트롤러 요청");
-		String result = "";
-		
-		String nowpwd = now_pwd; 	// 현재 비밀번호 받아오기
-		String newpwd = new_pwd1;	// 새로운 비밀번호 받아오기
-		
-		//System.out.println("현재 비밀번호 확인 : " + nowpwd);
-		//System.out.println("새로운 비밀번호 확인 : " + newpwd);
-		
-		String pwdCheck = "";
-		
-		String pwd = "userpwd1";	// us_code 2393726 의 비밀번호 복호화 키
-		
-		try
+		// 플랫폼 비밀번호 수정
+		@RequestMapping(value = "/platformpwdupdate.woori", method = RequestMethod.POST)
+		public String pwdUpdate(HttpSession session , @RequestParam("now_pwd") String now_pwd, @RequestParam("new_pwd1") String new_pwd1)
 		{
+			UserDTO sessiondto = (UserDTO) session.getAttribute("userDTO");
+			String us_code = sessiondto.getUs_code();
+			String us_pwd = sessiondto.getUs_pwd();		// 세션으로 us_code 받아오기
+			//System.out.println("세션에서 받아온 문자열 : " + us_code);
+			//System.out.println("인트로 형변환 : " + Integer.parseInt(us_code));
+			
+			
+			//System.out.println("컨트롤러 요청"); 확인
+			
+			String result = "";
+			
+			String nowpwd = now_pwd; 	// 현재 비밀번호 받아오기
+			String newpwd = new_pwd1;	// 새로운 비밀번호 받아오기
+			
+			//System.out.println("현재 비밀번호 확인 : " + nowpwd);
+			//System.out.println("새로운 비밀번호 확인 : " + newpwd);
+			
+			String pwdCheck = "";
+			
+			String session_pwd = us_pwd;	// 세션으로 받아 온 us_pwd
+			
+			try
+			{
+				UserAccountDAO dao = new UserAccountDAO();
+				//UserDTO dto = new UserDTO();
+				//System.out.println("dao 메소드로 들어가기 전 확인 : " + us_code);
+				pwdCheck = dao.pwdCheck(now_pwd, us_code);	// 복호화가 되면 userpwd1 , 안되면 특수문자가 출력된 값을 pwdCheck 변수에 담기
+				//System.out.println("컨트롤러에서 복호화된 비밀번호 확인 : " + pwdCheck);
+				
+				if (now_pwd.equals(pwdCheck))		// 입력한 현재 비번 (userpwd1) = pwdCheck메소드로 복호화된 비번(userpwd1)이 같으면 수정 메소드 호출
+				{
+					//System.out.println("수정 과정 거침");
+					//dto.setUs_pwd(newpwd);
+					//dto.setUs_pwd2(newpwd);
+					dao.pwdModify(newpwd, us_code);
+				}
+				else							// 같지 않다면 안돼
+					System.out.println("ㅋㅋ안됨");
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			result = "redirect:user.woori";
+			return result; 
+		}
+		
+	// ====================================================================================
+		// 플랫폼 탈퇴 요청 관련 액션 처리
+
+		@RequestMapping(value = "/accountwithdrawlform.woori")
+		public String accountRemoveForm(Model model, HttpSession session)
+		{
+			
+			UserDTO sessiondto = (UserDTO) session.getAttribute("userDTO");
+			String us_code = sessiondto.getUs_code();
+			
+			
+			UserAccountDAO dao = new UserAccountDAO();
+			int userPos = 0;
+			UserDTO userId = new UserDTO();
+			
+			try
+			{
+				userPos = dao.count(us_code);
+				//userId = dao.myProfile();
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			// 아이디 보내기
+			model.addAttribute("userId", userId);
+			// 직위가 있는지 확인 메소드 보내기
+			model.addAttribute("userPos", userPos);
+			return "/WEB-INF/view/AccountWithdrawal.jsp";
+		}
+		
+		
+		@RequestMapping(value = "/accountwithdrawl.woori",  method=RequestMethod.POST)
+		public String accountRemove(HttpSession session)
+		{
+			UserDTO sessiondto = (UserDTO) session.getAttribute("userDTO");
+			String us_code = sessiondto.getUs_code();
+			
+			String result = "";
+			
 			UserAccountDAO dao = new UserAccountDAO();
 			//UserDTO dto = new UserDTO();
 			
-			pwdCheck = dao.pwdCheck(pwd);	// 복호화가 되면 userpwd1 , 안되면 특수문자가 출력된 값을 pwdCheck 변수에 담기
-			//System.out.println("컨트롤러에서 복호화된 비밀번호 확인 : " + pwdCheck);
-			
-			if (now_pwd.equals(pwdCheck))		// 입력한 현재 비번 (userpwd1) = pwdCheck메소드로 복호화된 비번(userpwd1)이 같으면 수정 메소드 호출
+			try
 			{
-				//System.out.println("수정 과정 거침");
-				//dto.setUs_pwd(newpwd);
-				//dto.setUs_pwd2(newpwd);
-				dao.pwdModify(newpwd);
+				dao.accountWthdr(us_code);
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
 			}
-			else							// 같지 않다면 안돼
-				System.out.println("안됨");
 			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
+			result = "redirect:loginform.woori";
+			return result;
 		}
-		result = "redirect:user.woori";
-		return result; 
-	}
-	
-	// ====================================================================================
-	// 플랫폼 탈퇴 요청 관련 액션 처리
-
-	@RequestMapping("/accountwithdrawlform.woori")
-	public String accountRemoveForm(Model model)
-	{
-		UserAccountDAO dao = new UserAccountDAO();
-		int userPos = 0;
-		UserDTO userId = new UserDTO();
-		
-		try
-		{
-			userPos = dao.count();
-			userId = dao.myProfile();
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		// 아이디 보내기
-		model.addAttribute("userId", userId);
-		// 직위가 있는지 확인 메소드 보내기
-		model.addAttribute("userPos", userPos);
-		return "/WEB-INF/view/AccountWithdrawal.jsp";
-	}
-	
-	
-	@RequestMapping(value = "/accountwithdrawl.woori",  method=RequestMethod.POST)
-	public String accountRemove()
-	{
-		String result = "";
-		
-		UserAccountDAO dao = new UserAccountDAO();
-		UserDTO dto = new UserDTO();
-		
-		try
-		{
-			dao.accountWthdr(dto);
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		result = "redirect:user.woori";
-		return result;
-	}
-	
 	//======================================================================================
 
 
--- Woori-man/src/com/woori/dao/UserAccountDAO.java
+++ Woori-man/src/com/woori/dao/UserAccountDAO.java
@@ -17,263 +17,233 @@ import com.woori.util.DBConn;
 public class UserAccountDAO
 {
 	// 나의 플랫폼 계정정보 조회
-	public UserDTO myProfile() throws SQLException, ClassNotFoundException
-	{
-		// System.out.println("--------------");
-		UserDTO result = new UserDTO();
-
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		//○ 회원이름, 회원아이디, 회원프로필사진, 회원전화번호, 회원이메일, 회원주소
-		// → 3/7 회원 프로필 사진 데이터 추가
-		String sql = "SELECT US_NAME, US_ID, US_PROFILE, US_TEL, US_EMAIL, US_ZIPCODE, US_ADDR1, US_ADDR2 FROM VIEW_USER WHERE US_CODE=3859105";
-
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		
-		//pstmt.setInt(1, Integer.parseInt(us_Code));
-
-		ResultSet rs = pstmt.executeQuery();
-
-		while (rs.next())
+		public UserDTO myProfile(String us_code) throws SQLException, ClassNotFoundException
 		{
-			result.setUs_name(rs.getString("US_NAME"));
-			result.setUs_id(rs.getString("US_ID"));
-			result.setUs_profile(rs.getString("US_PROFILE"));
-			result.setUs_tel(rs.getString("US_TEL"));
-			result.setUs_email(rs.getString("US_EMAIL"));
-			
-			result.setUs_zipcode(rs.getString("US_ZIPCODE"));
-			result.setUs_addr1(rs.getString("US_ADDR1"));
-			result.setUs_addr2(rs.getString("US_ADDR2"));
-			
-			// System.out.println("--------------"+ rs.getString("US_EMAIL"));
-		}
+			// System.out.println("--------------");
+			UserDTO result = new UserDTO();
 
-		rs.close();
-		pstmt.close();
-
-		DBConn.close();
-
-		return result;
-	}
-
-	// 플랫폼 계정정보 수정 → 3/7 → 프로필 사진 데이터 추가
-	// ○ 회원프사, 회원전화번호, 회원주소, 회원이메일, 양음력
-	public int modify(UserDTO dto) throws SQLException, ClassNotFoundException
-	{
-		int result = 0;
-
-		try
-		{
-			Connection conn = null;
-			conn = DBConn.getConnection();
-
-			String sql = "UPDATE USER_INFO SET US_PROFILE = ?, US_TEL = ?, US_ZIPCODE = ?, US_ADDR1 = ?, US_ADDR2 = ? "
-						+ " ,US_EMAIL = ?, US_LUNAR = ? WHERE US_CODE = 3859105";
-
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt = conn.prepareStatement(sql);
-			
-			//CallableStatement cstmt = null;
-			//cstmt = conn.prepareCall(sql);
-			
-			pstmt.setString(1, dto.getUs_profile());
-			pstmt.setString(2, dto.getUs_tel());
-			pstmt.setString(3, dto.getUs_zipcode());
-			pstmt.setString(4, dto.getUs_addr1());
-			pstmt.setString(5, dto.getUs_addr2());
-			pstmt.setString(6, dto.getUs_email());
-			pstmt.setString(7, dto.getUs_lunar());
-
-			result = pstmt.executeUpdate();
-
-			pstmt.close();
-			DBConn.close();
-
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		// Connection conn = dataSource.getConnection();
-		
-		return result;
-	}
-	
-	// 입력한 비밀번호가 현재 비밀번호와 같은지 확인하는 메소드 → 풀리면 숫자, 안풀리면 특수문자가 보임
-	public String pwdCheck(String us_pwd)
-	{
-		String result = "";
-		//us_pwd = "2393726";
-		
-		try
-		{
 			Connection conn = null;
 			conn = DBConn.getConnection();
 			
-			// userpwd1 입력 시 → 6627127(암호화안풀림 ) / 2393726(암호화풀림)
-			String sql = "SELECT CRYPTPACK.DECRYPT(US_PWD, ?) FROM USERS WHERE US_CODE = 2393726";
-			
+			//○ 회원이름, 회원아이디, 회원프로필사진, 회원전화번호, 회원이메일, 회원주소
+			// → 3/7 회원 프로필 사진 데이터 추가
+			String sql = "SELECT US_NAME, US_ID, US_PROFILE, US_TEL, US_EMAIL, US_ZIPCODE, US_ADDR1, US_ADDR2 FROM USER_VIEW_PLATFORM WHERE US_CODE=?";
+
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, us_pwd);
-			
+			pstmt.setString(1, us_code);
+
 			ResultSet rs = pstmt.executeQuery();
+
 			while (rs.next())
 			{
-				result = rs.getString(1);
-			}
-			
-			rs.close();
-			pstmt.close();
-			DBConn.close();
-			
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		System.out.println("check 메소드 복호화 확인 : " + result);
-		
-		
-		return result;
-	}
-	
-	// 플랫폼 비밀번호 수정과 동시에 암호화 처리
-	public int pwdModify(String us_pwd)
-	{
-		int result = 0;
-		
-		try
-		{
-			Connection conn = null;
-			conn = DBConn.getConnection();
-			//UserDTO dto = new UserDTO();
-			
-			//dto.setUs_pwd(us_pwd);
-			//dto.setUs_pwd2(us_pwd);
-			
-			String sql = "UPDATE USERS SET US_PWD = CRYPTPACK.ENCRYPT(?, ?) WHERE US_CODE = 2393726";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
-			//pstmt.setString(1, dto.getUs_pwd());	// 새로운 비밀번호
-			//pstmt.setString(2, dto.getUs_pwd2());   // 새로운 비밀번호 복호화 키
-			pstmt.setString(1, us_pwd);	// 새로운 비밀번호
-			pstmt.setString(2, us_pwd);   // 새로운 비밀번호 복호화 키
-			
-			result = pstmt.executeUpdate();
-			
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		System.out.println("modify 메소드 수정 비밀번호 확인 : " + us_pwd);
-		
-		return result;
-	
-	}
-	
-
-	// 플랫폼 비밀번호 수정
-	public int pwdModify(UserDTO dto)
-	{
-		int result = 0;
-		
-		try
-		{
-			Connection conn = null;
-			conn = DBConn.getConnection();
-			
-			String sql = "{call PRC_USERPWD_UPDATE (?, ?, ?)}";
-			
-			CallableStatement cstmt = null;
-			cstmt = conn.prepareCall(sql);
-			
-			cstmt.setString(1, dto.getUs_code());
-			cstmt.setString(2, dto.getUs_pwd());
-			//cstmt.setString(3, dto.getUs_pwd2());
-			cstmt.setString(3, dto.getUs_pwd());
-			
-			result = cstmt.executeUpdate();
-			
-			cstmt.close();
-			DBConn.close();
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		return result;
-	}
-	
-	
-	
-	// 탈퇴 전 직위가 있는지 카운트 (확인○)
-	public int count()
-	{
-		int result = 0;
-		
-		try
-		{
-			Connection conn = null;
-			conn = DBConn.getConnection();
-			
-			// 3859105 탈퇴버튼활성화 , 9513969(여행그룹장) 탈퇴버튼비활성화
-			String sql = "SELECT COUNT(*) AS COUNT FROM VIEW_USERPOSITION WHERE US_CODE = 9513969";
-			
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			
-			if (rs.next())
-			{
-				result = rs.getInt("COUNT");
-			}
-			
-			rs.close();
-			pstmt.close();
-			DBConn.close();
-			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		return result;
-	}
-	
-	
-	// 플랫폼 탈퇴 (테스트아직X)
-	public int accountWthdr(UserDTO dto)
-	{
-		int result = 0;
-		
-		try
-		{
-			Connection conn = null;
-			conn = DBConn.getConnection();
-			
-			String sql = "{call PRC_USER_WHTDR(?)}";
-			
-			CallableStatement cstmt = conn.prepareCall(sql);
-			
-			cstmt.setString(1, dto.getUs_code());
+				result.setUs_name(rs.getString("US_NAME"));
+				result.setUs_id(rs.getString("US_ID"));
+				result.setUs_profile(rs.getString("US_PROFILE"));
+				result.setUs_tel(rs.getString("US_TEL"));
+				result.setUs_email(rs.getString("US_EMAIL"));
+				result.setUs_zipcode(rs.getString("US_ZIPCODE"));
+				result.setUs_addr1(rs.getString("US_ADDR1"));
+				result.setUs_addr2(rs.getString("US_ADDR2"));
 				
-			result = cstmt.executeUpdate();
-			
-			cstmt.close();
+				// System.out.println("--------------"+ rs.getString("US_EMAIL"));
+			}
+			rs.close();
+			pstmt.close();
+
 			DBConn.close();
-			
-			
-		} catch (Exception e)
+
+			return result;
+		}
+
+		// 플랫폼 계정정보 수정 → 3/7 → 프로필 사진 데이터 추가
+		// ○ 회원프사, 회원전화번호, 회원주소, 회원이메일
+		public int modify(UserDTO dto) throws SQLException, ClassNotFoundException
 		{
-			System.out.println(e.toString());
+			int result = 0;
+
+			try
+			{
+				Connection conn = null;
+				conn = DBConn.getConnection();
+
+				String sql = "UPDATE USER_INFO SET US_PROFILE = ?, US_TEL = ?, US_ZIPCODE = ?, US_ADDR1 = ?, US_ADDR2 = ? ,US_EMAIL = ? WHERE US_CODE = ?";
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(1, dto.getUs_profile());
+				pstmt.setString(2, dto.getUs_tel());
+				pstmt.setString(3, dto.getUs_zipcode());
+				pstmt.setString(4, dto.getUs_addr1());
+				pstmt.setString(5, dto.getUs_addr2());
+				pstmt.setString(6, dto.getUs_email());
+				pstmt.setString(7, dto.getUs_code());
+
+				result = pstmt.executeUpdate();
+
+				pstmt.close();
+				DBConn.close();
+
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			// Connection conn = dataSource.getConnection();
+			
+			return result;
+		}
+	
+		// 입력한 비밀번호가 현재 비밀번호와 같은지 확인하는 메소드 → 풀리면 숫자, 안풀리면 특수문자가 보임(완료)
+		public String pwdCheck(String us_pwd, String us_code)
+		{
+			String result = "";
+			//us_pwd = "2393726";
+			
+			try
+			{
+				Connection conn = null;
+				conn = DBConn.getConnection();
+				
+				// userpwd1 입력 시 → 6627127(암호화안풀림 ) / 2393726(암호화풀림)
+			
+				String sql = "SELECT CRYPTPACK.DECRYPT(US_PWD, ?) FROM USERS WHERE US_CODE = ?";
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, us_pwd);
+				pstmt.setInt(2, Integer.parseInt(us_code));
+				System.out.println("us_code 구성 완료~");
+				
+				ResultSet rs = pstmt.executeQuery();
+				System.out.println("쿼리문 날리기!!");
+				while (rs.next())
+				{
+					result = rs.getString(1);
+					//UserDTO dto = new UserDTO();
+					//dto.setUs_pwd(rs.getString(us_pwd));
+				}
+				
+				rs.close();
+				pstmt.close();
+				DBConn.close();
+				
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			System.out.println("check 메소드 복호화 확인 : " + result);
+			
+			return result;
+		}
+	
+		// 플랫폼 비밀번호 수정과 동시에 암호화 처리(완료)
+		public int pwdModify(String us_pwd, String us_code)
+		{
+			int result = 0;
+			
+			try
+			{
+				Connection conn = null;
+				conn = DBConn.getConnection();
+				
+				//UserDTO dto = new UserDTO();
+				//dto.setUs_pwd(us_pwd);
+				//dto.setUs_pwd2(us_pwd);
+				
+				String sql = "UPDATE USERS SET US_PWD = CRYPTPACK.ENCRYPT(?, ?) WHERE US_CODE = ?";
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				
+				//pstmt.setString(1, dto.getUs_pwd());	// 새로운 비밀번호
+				//pstmt.setString(2, dto.getUs_pwd2());   // 새로운 비밀번호 복호화 키
+				
+				pstmt.setString(1, us_pwd);		// 새로운 비밀번호
+				pstmt.setString(2, us_pwd);   // 새로운 비밀번호 복호화 키
+				pstmt.setInt(3, Integer.parseInt(us_code));
+				
+				result = pstmt.executeUpdate();
+				
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			System.out.println("modify 메소드 수정 비밀번호 확인 : " + us_pwd);
+			
+			return result;
+		
 		}
 		
-		return result;
-	}
+
 	
+	
+		// 탈퇴 전 직위가 있는지 카운트 (확인○)
+		public int count(String us_code)
+		{
+			int result = 0;
+			
+			try
+			{
+				Connection conn = null;
+				conn = DBConn.getConnection();
+				
+				// 방탈 그룹장이 회원인 경우. .탈퇴 버튼 비활성화되어야 함 →
+				// 문남숙>? 이 회원인 경우 탈퇴 버튼 활성화되어야 함
+				String sql = "SELECT COUNT(*) AS COUNT FROM USERPOSITION_VIEW WHERE US_CODE = ?";
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, Integer.parseInt(us_code));
+				
+				ResultSet rs = pstmt.executeQuery();
+				
+				if (rs.next())
+				{
+					result = rs.getInt("COUNT");
+				}
+				
+				rs.close();
+				pstmt.close();
+				DBConn.close();
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			return result;
+		}
+	
+		// 플랫폼 탈퇴 (완료)
+		public int accountWthdr(String us_code)
+		{
+			int result = 0;
+			
+			try
+			{
+				Connection conn = null;
+				conn = DBConn.getConnection();
+				
+				String sql = "{call PRC_USER_WHTDR(?)}";
+				
+				CallableStatement cstmt = conn.prepareCall(sql);
+				
+				cstmt.setInt(1, Integer.parseInt(us_code));
+					
+				result = cstmt.executeUpdate();
+				
+				cstmt.close();
+				DBConn.close();
+				
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			return result;
+		}
+		
 	
 	
 
