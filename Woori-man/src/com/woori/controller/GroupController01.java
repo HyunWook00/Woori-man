973e9ed746bf0353a0077b55d32772317bade4e6
박가영 <g.park1995@gmail.com>
2024-03-16 13:28:05 +0900

    2024-03-18 박가영 GroupListDAO, GroupController01

--- Woori-man/src/com/woori/controller/GroupController01.java
+++ Woori-man/src/com/woori/controller/GroupController01.java
@@ -9,12 +9,15 @@ import java.sql.SQLException;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.woori.dao.GroupListDAO;
 import com.woori.dto.GroupDTO;
+import com.woori.dto.UserDTO;
 
 
 
@@ -27,52 +30,58 @@ public class GroupController01
 {
 	
 	//나의 그룹 리스트
-	@RequestMapping(value = "/group.woori")
-	public String GroupList(Model model) throws ClassNotFoundException, SQLException
-	{
-		GroupListDAO dao = new GroupListDAO();
-		
-		ArrayList<GroupDTO> myGroupList = new ArrayList<GroupDTO>();		// 나의 가입그룹 리스트
-		ArrayList<GroupDTO> waitingGroupList = new ArrayList<GroupDTO>();   // 나의 대기그룹 리스트
-		
-		ArrayList<GroupDTO> inviteGroupList = new ArrayList<GroupDTO>();    // 초대 그룹 리스트
-		
-		GroupDTO leaderName = new GroupDTO();								// 그룹장 닉네임
+		@RequestMapping(value = "/group.woori")
+		public String GroupList(Model model, HttpSession session) throws ClassNotFoundException, SQLException
+		{
+			GroupListDAO dao = new GroupListDAO();
+			
+			UserDTO dto = (UserDTO) session.getAttribute("userDTO");
+			
+			
+			ArrayList<GroupDTO> myGroupList = new ArrayList<GroupDTO>();		// 나의 가입그룹 리스트
+			ArrayList<GroupDTO> waitingGroupList = new ArrayList<GroupDTO>();   // 나의 대기그룹 리스트
+			ArrayList<GroupDTO> inviteGroupList = new ArrayList<GroupDTO>();    // 초대 그룹 리스트
+			ArrayList<GroupDTO> blockedGroupList = new ArrayList<GroupDTO>();   // 차단 그룹 리스트
+			
+			//GroupDTO leaderName = new GroupDTO();								// 그룹장 닉네임
 
-		//GroupDTO recentDate = new GroupDTO();
-		
-		int us_code = 3859105;
-		int cg_code = 5;
-		
-		try
-		{
-			myGroupList = dao.myGrouplist(us_code);
-			waitingGroupList = dao.waitingGroupList(us_code);
-			inviteGroupList = dao.inviteGroupList(us_code);
+			//GroupDTO recentDate = new GroupDTO();
 			
-			leaderName = dao.groupLeader(cg_code);
-			//recentDate = dao.recentDate();
+			String us_code = dto.getUs_code();
+			//int us_code = 3859105;
+			//int cg_code = 5;
 			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
+			try
+			{
+				myGroupList = dao.myGrouplist(us_code);
+				waitingGroupList = dao.waitingGroupList(us_code);
+				inviteGroupList = dao.inviteGroupList(us_code);
+				blockedGroupList = dao.blockedGroupList(us_code);
+				
+				//leaderName = dao.groupLeader(cg_code);
+				//recentDate = dao.recentDate();
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			//System.out.println(inviteGroupList.size());
+			
+			model.addAttribute("myGroupList" , myGroupList);			// 가입 그룹 리스트 
+			model.addAttribute("waitingGroupList", waitingGroupList);   // 대기 그룹 리스트 
+			model.addAttribute("inviteGroupList", inviteGroupList);     // 초대 그룹 리스트 
+			model.addAttribute("blockedGroupList", blockedGroupList);	// 차단 그룹 리스트
+			
+			//model.addAttribute("leaderName", leaderName);     // 그룹장 닉네임 
+			
+			
+			//model.addAttribute("recentDate", recentDate);
+			
+			
+			return "/WEB-INF/view/TotalGroupList2.jsp";
+			
 		}
-		
-		//System.out.println(inviteGroupList.size());
-		
-		model.addAttribute("myGroupList" , myGroupList);			// 가입 그룹 리스트 
-		model.addAttribute("waitingGroupList", waitingGroupList);   // 대기 그룹 리스트 
-		model.addAttribute("inviteGroupList", inviteGroupList);     // 초대 그룹 리스트 
-		
-		model.addAttribute("leaderName", leaderName);     // 그룹장 닉네임 
-		
-		
-		//model.addAttribute("recentDate", recentDate);
-		
-		
-		return "/WEB-INF/view/TotalGroupList2.jsp";
-		
-	}
 	
 	
 	// 나의 가입 그룹의 최근 활동일 -- 어케 컨트롤러 두 개를 받을지..몰게삼 DAO를 바꿔야하낭
@@ -156,80 +165,112 @@ public class GroupController01
 	
 	
 	
-	// 수락 버튼 클릭 시 (테스트 완료, 보드게임모임 대기그룹리스트로 이동)
-	@RequestMapping("/invitationacccept.woori")
-	public String invitationAccept(Model model)
-	{
-		// 보드게임모임 수락
-		int gi_code = 25;
-		
-		GroupListDAO dao = new GroupListDAO();
-		
-		int accept = 0;
-		
-		try
+		// 수락 버튼 클릭 시 (테스트 완료, 보드게임모임 대기그룹리스트로 이동)
+		@RequestMapping("/invitationacccept.woori")
+		public String invitationAccept(Model model, HttpSession session)
 		{
-			accept = dao.accept(gi_code);
+			GroupDTO dto = (GroupDTO) session.getAttribute("GroupDTO");
+			String gi_code = dto.getGi_code(); 
 			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		model.addAttribute("accept", accept);
-		
-		
-		return "group.woori";
-	}
-	
-	
-	// 거절 버튼 클릭 시 (테스트 완료)
-	@RequestMapping("/invitationrefuse.woori")
-	public String invitationRefuse(Model model)
-	{
-		// 부장모임 거절
-		int gi_code = 18;
-		
-		GroupListDAO dao = new GroupListDAO();
-		
-		int refuse = 0;
-		
-		try
-		{
-			refuse = dao.refuse(gi_code);
+			GroupListDAO dao = new GroupListDAO();
 			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-		
-		model.addAttribute("refuse", refuse);
-		
-		return "group.woori";
-	}
-	
-	// 차단하시겠습니까? 클릭시
-	/*
-	public String invitationBlock(Model model)
-	{
-		// 새로운 차단 모임 테스트
-		int cg_code = ; 
-		int us_code = 3859105;
-		
-		GroupListDAO dao = new GroupListDAO();
-		
-		int block = 0;
-		
-		try
-		{
-			block = dao.block(cg_code, us_code);
+			int accept = 0;
 			
-		} catch (Exception e)
-		{
-			System.out.println(e.toString());
+			try
+			{
+				accept = dao.accept(gi_code);
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			model.addAttribute("accept", accept);
+			
+			return "group.woori";
 		}
 		
+		
+		// 거절 버튼 클릭 시 (테스트 완료)
+		// 초대 거절 → 거절만 할게요 버튼 클릭 시 (그룹 초대 테이블) 업데이트
+		@RequestMapping(value = "/invitationrefuse.woori")
+		public String invitationRefuse(Model model, HttpSession session)
+		{
+			GroupDTO dto = (GroupDTO) session.getAttribute("GroupDTO");
+			String gi_code = dto.getGi_code(); 
+			
+			GroupListDAO dao = new GroupListDAO();
+			
+			int refuse = 0;
+			
+			try
+			{
+				refuse = dao.refuse(gi_code);
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			model.addAttribute("refuse", refuse);
+			
+			return "group.woori";
 		}
-		*/
+		
+		
+		// 차단하시겠습니까? 클릭시
+		// 초대 거절 → 차단도 할래요 버튼 클릭시 (그룹 초대 테이블 + 그룹 차단 테이블) 업데이트
+		@RequestMapping(value = "/invitationblock.woori")
+		public String invitationBlock(Model model, HttpSession session)
+		{
+			GroupDTO dto = (GroupDTO) session.getAttribute("GroupDTO");
+			String gi_code = dto.getGi_code(); 
+			String cg_code = dto.getCg_code();
+			String us_code = dto.getUs_code();
+			
+			GroupListDAO dao = new GroupListDAO();
+			
+			int block = 0;
+			int refuse = 0;
+			
+			try
+			{
+				refuse = dao.refuse(gi_code);				// 초대거절메소드
+				block = dao.block(us_code, cg_code);		// 그룹차단메소드	
+				
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			model.addAttribute("refuse", refuse);
+			model.addAttribute("block", block);
+			
+			return "group.woori";
+		}
+		
+		// 차단그룹 목록에서 차단해제 버튼 클릭시 (그룹차단 테이블 삭제)
+		@RequestMapping(value = "invitaionUnblock.woori")
+		public String invitationUnBlock(Model model, HttpSession session)
+		{
+			GroupDTO dto = (GroupDTO) session.getAttribute("GroupDTO");
+			String cg_code = dto.getCg_code();
+			String us_code = dto.getUs_code();
+			
+			GroupListDAO dao = new GroupListDAO();
+			
+			int unblock = 0;
+			
+			try
+			{
+				unblock = dao.unblock(us_code, cg_code);
+			} catch (Exception e)
+			{
+				System.out.println(e.toString());
+			}
+			
+			return "group.woori";
+			
+		}
 
 }
--- Woori-man/src/com/woori/dao/GroupListDAO.java
+++ Woori-man/src/com/woori/dao/GroupListDAO.java
@@ -29,43 +29,43 @@ public class GroupListDAO
 	// 3/6 그룹 검색 메소드 추가
 
 	// 나의 가입그룹 상세조회 리스트 → 폐쇄된 그룹인지 확인 후 조회되도록 수정
-	public ArrayList<GroupDTO> myGrouplist(int us_code) throws SQLException, ClassNotFoundException
-	{
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
-		
-		//Connection conn = dataSource.getConnection();
-		
-		// ○그룹명, 그룹프사, 그룹멤버등록일, 그룹카테고리, 그룹장닉네임, 그룹최근활동일, 그룹한줄소개, 그룹즐겨찾기여부
-		// → 그룹장닉네임, 그룹최근활동일(메소드추가), 그룹즐겨찾기 여부(메소드추가)
-		String sql = "SELECT CG_NAME, CG_PROFILE, GM_REGDATE, GC_NAME, CG_INTRO FROM VIEW_USERGROUPLIST WHERE US_CODE = ?";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, us_code);
-		ResultSet rs = pstmt.executeQuery();
-		
-		while (rs.next())
+		public ArrayList<GroupDTO> myGrouplist(String us_code) throws SQLException, ClassNotFoundException
 		{
-			GroupDTO dto = new GroupDTO();
-			dto.setCg_name(rs.getString("CG_NAME"));
-			dto.setCg_profile(rs.getString("CG_PROFILE"));
-			dto.setGm_regDate(rs.getString("GM_REGDATE"));
-			dto.setGc_name(rs.getString("GC_NAME"));			
-			dto.setCg_intro(rs.getString("CG_INTRO"));
+			Connection conn = null;
+			conn = DBConn.getConnection();
 			
-			result.add(dto);
+			ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
+			
+			// ○그룹명, 그룹프사, 그룹멤버등록일, 그룹카테고리, 그룹장닉네임, 그룹한줄소개, 그룹즐겨찾기여부
+			// → 그룹장닉네임, 그룹즐겨찾기 여부(메소드추가)
+			String sql = "SELECT CG_NAME, CG_PROFILE, GC_NAME, CG_INTRO FROM USERGROUPLIST_VIEW WHERE US_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,us_code);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next())
+			{
+				GroupDTO dto = new GroupDTO();
+				
+				dto.setCg_name(rs.getString("CG_NAME"));
+				dto.setCg_profile(rs.getString("CG_PROFILE"));
+				
+				dto.setGc_name(rs.getString("GC_NAME"));			
+				dto.setCg_intro(rs.getString("CG_INTRO"));
+				
+				result.add(dto);
+			}
+			
+			
+			rs.close();
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
 		}
 		
-		rs.close();
-		pstmt.close();
-		DBConn.close();
-		
-		return result;
-	}
-	
-	
+	/*
 	// 내 그룹의 그룹장 닉네임 조회
 	public GroupDTO groupLeader(int cg_code) throws ClassNotFoundException, SQLException
 	{
@@ -125,161 +125,215 @@ public class GroupListDAO
 		return result;
 		
 	}
+	*/
 	
 	// 대기 그룹 리스트 조회  → 그룹원 정보 등록 버튼 클릭시 그룹메인으로 이동
-	public ArrayList<GroupDTO> waitingGroupList(int us_code) throws ClassNotFoundException, SQLException
-	{
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
-		
-		//○ 그룹명, 그룹프사, 그룹카테고리, 그룹장닉네임, 그룹한줄소개, 그룹초대응답일
-		// → 그룹카테고리, 그룹장닉네임 추가
-		/*
-		 * String sql = "SELECT CG_PROFILE, CG_NAME, CG_INTRO, GI_RESPONSE " +
-		 * " FROM VIEW_USERGROUPLIST WHERE US_CODE =? AND RS_CODE = 1 AND GI_RESPONSE IS NOT NULL AND GM_REGDATE IS NULL"
-		 * ;
-		 */
-		
-		String sql = "SELECT CG_NAME, CG_PROFILE, GC_NAME, CG_INTRO, GI_RESPONSE FROM VIEW_WAITINGGROUPLIST WHERE US_CODE = ?";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, us_code);
-		ResultSet rs = pstmt.executeQuery();
-		
-		while (rs.next())
+		public ArrayList<GroupDTO> waitingGroupList(String us_code) throws ClassNotFoundException, SQLException
 		{
-			GroupDTO dto = new GroupDTO();
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
+			
+			//○ 그룹명, 그룹프사, 그룹카테고리, 그룹장닉네임, 그룹한줄소개, 그룹초대응답일
+			// → 그룹카테고리, 그룹장닉네임 추가
+			/*
+			 * String sql = "SELECT CG_PROFILE, CG_NAME, CG_INTRO, GI_RESPONSE " +
+			 * " FROM VIEW_USERGROUPLIST WHERE US_CODE =? AND RS_CODE = 1 AND GI_RESPONSE IS NOT NULL AND GM_REGDATE IS NULL"
+			 * ;
+			 */
 			
 			
-			dto.setCg_name(rs.getString("CG_NAME"));
-			dto.setCg_profile(rs.getString("CG_PROFILE"));
-			dto.setGc_name(rs.getString("GC_NAME"));			
-			dto.setCg_intro(rs.getString("CG_INTRO"));
-			dto.setGi_response(rs.getString("GI_RESPONSE"));
-			//dto.setGm_Nickname(rs.getString("GM_NICKNAME"));
+			//String sql = "SELECT CG_NAME, CG_PROFILE, GC_NAME, CG_INTRO, GI_RESPONSE FROM VIEW_WAITINGGROUPLIST WHERE US_CODE = ?";
+			String sql = "SELECT CG_NAME, CG_PROFILE, GC_NAME, CG_INTRO FROM WAITINGGROUPLIST_VIEW WHERE US_CODE = ?";
 			
-			result.add(dto);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,us_code);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next())
+			{
+				GroupDTO dto = new GroupDTO();
+				
+				
+				dto.setCg_name(rs.getString("CG_NAME"));
+				dto.setCg_profile(rs.getString("CG_PROFILE"));
+				dto.setGc_name(rs.getString("GC_NAME"));			
+				dto.setCg_intro(rs.getString("CG_INTRO"));
+				//dto.setGi_response(rs.getString("GI_RESPONSE"));
+				//dto.setGm_Nickname(rs.getString("GM_NICKNAME"));
+				
+				result.add(dto);
+			}
+			
+			rs.close();
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
 		}
 		
-		rs.close();
-		pstmt.close();
-		DBConn.close();
 		
-		return result;
-	}
-	
-	
-	// 초대 받은 그룹 리스트 조회 → 차단하지 않은 그룹만 조회 추가완료
-	public ArrayList<GroupDTO> inviteGroupList(int us_code) throws ClassNotFoundException, SQLException
-	{
-		
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		//int us_Code = 1;
-		
-		ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
-		
-		//○ 그룹명, 그룹프사, 그룹장닉네임, 그룹한줄소개, 그룹초대발신일
-		//→ 그룹장닉네임, 그룹초대발신일 추가
-		
-		String sql = "SELECT CG_NAME, CG_PROFILE, CG_INTRO, GI_REQUEST FROM VIEW_INVITEGROUP WHERE US_CODE = ?";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		
-		pstmt.setInt(1, us_code);
-		
-		ResultSet rs = pstmt.executeQuery();
-		
-		while (rs.next())
+		// 초대 받은 그룹 리스트 조회 → 차단하지 않은 그룹만 조회 추가완료
+		public ArrayList<GroupDTO> inviteGroupList(String us_code) throws ClassNotFoundException, SQLException
 		{
-			GroupDTO dto = new GroupDTO();
+			Connection conn = null;
+			conn = DBConn.getConnection();
 			
-			dto.setCg_name(rs.getString("CG_NAME"));
-			dto.setCg_profile(rs.getString("CG_PROFILE"));
-			dto.setCg_intro(rs.getString("CG_INTRO"));
-			dto.setGi_request(rs.getString("GI_REQUEST"));
+			//int us_Code = 1;
 			
-			result.add(dto);
+			ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
+			
+			//○ 그룹명, 그룹프사, 그룹장닉네임, 그룹한줄소개, 그룹초대발신일
+			//→ 그룹장닉네임, 그룹초대발신일 추가
+			String sql = "SELECT CG_NAME, CG_PROFILE, CG_INTRO FROM VIEW_INVITEGROUP WHERE US_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1,Integer.parseInt(us_code));
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next())
+			{
+				GroupDTO dto = new GroupDTO();
+				dto.setCg_name(rs.getString("CG_NAME"));
+				dto.setCg_profile(rs.getString("CG_PROFILE"));
+				dto.setCg_intro(rs.getString("CG_INTRO"));
+				//dto.setGi_request(rs.getString("GI_REQUEST"));
+				
+				result.add(dto);
+			}
+			
+			rs.close();
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
 		}
 		
-		rs.close();
-		pstmt.close();
-		DBConn.close();
+		// 차단한 그룹리스트 조회
+		public ArrayList<GroupDTO> blockedGroupList(String us_code) throws ClassNotFoundException, SQLException
+		{
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			ArrayList<GroupDTO> result = new ArrayList<GroupDTO>();
+			
+			String sql = "SELECT CG_NAME, CG_INTRO, GC_NAME FROM BLOCKEDGROUP_VIEW WHERE US_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, us_code);
+			
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next())
+			{
+				GroupDTO dto = new GroupDTO();
+				dto.setCg_name(rs.getString("CG_NAME"));
+				dto.setCg_intro(rs.getString("CG_INTRO"));
+				dto.setGc_name(rs.getString("GC_NAME"));
+				
+				result.add(dto);
+			}
+			
+			rs.close();
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
+		}
 		
-		
-		return result;
-	}
 	
 	
-	// 초대 수락 버튼 클릭 시 그룹 초대 테이블 업데이트  (테스트 ○)
-	public int accept(int gi_code) throws ClassNotFoundException, SQLException
-	{
-		int result = 0;
-		
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		String sql = "UPDATE GROUP_INVITE SET GI_RESPONSE = SYSDATE, RS_CODE = 1 WHERE GI_CODE = ?";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, gi_code);
-		
-		result = pstmt.executeUpdate();
-		
-		pstmt.close();
-		DBConn.close();
-		
-		return result;
-	}
+		// 초대 수락 버튼 클릭 시 그룹 초대 테이블 업데이트  (테스트 ○)
+		public int accept(String gi_code) throws ClassNotFoundException, SQLException
+		{
+			int result = 0;
+			
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			String sql = "UPDATE GROUP_INVITE SET GI_RESPONSE = SYSDATE, RS_CODE = 1 WHERE GI_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, gi_code);
+			
+			result = pstmt.executeUpdate();
+			
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
+		}
 
 
-	// 초대 거절 버튼 클릭 시 그룹 초대 테이블 업데이트 (테스트 X)
-	// + 그룹 → 그룹을 차단하시겠습니까? 추가 → 확인 시 그룹차단 테이블에 추가
-	public int refuse(int gi_code) throws ClassNotFoundException, SQLException
-	{
-		int result = 0;
+		// 초대 거절 버튼 클릭 시 그룹 초대 테이블 업데이트 (테스트 X)
+		// + 그룹 → 그룹을 차단하시겠습니까? 추가 → 확인 시 그룹차단 테이블에 추가
+		public int refuse(String gi_code) throws ClassNotFoundException, SQLException
+		{
+			int result = 0;
+			
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			
+			String sql = "UPDATE GROUP_INVITE SET GI_RESPONSE = SYSDATE, RS_CODE = 2 WHERE GI_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, gi_code);
+			
+			result = pstmt.executeUpdate();
+			
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
+		}
 		
-		Connection conn = null;
-		conn = DBConn.getConnection();
+		// 그룹 초대거절 → 차단할래요 버튼 클릭시 그룹차단테이블 업데이트
+		public int block(String us_code, String cg_code) throws ClassNotFoundException, SQLException
+		{
+			int result = 0;
+			
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			String sql = "INSERT INTO BLOCKED_GROUP(BG_CODE, CG_CODE, US_CODE, BG_DATE) VALUES(SEQ_BLOCKED_GROUP.NEXTVAL, ?, ?, SYSDATE)";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(us_code));
+			pstmt.setString(2, cg_code);
+			
+			result = pstmt.executeUpdate();
+			
+			pstmt.close();
+			DBConn.close();
+			
+			return result;
+		}
 		
+		// 그룹 차단 해제 버튼 클릭시 → 차단테이블에서 해당 그룹 삭제
+		public int unblock(String us_code, String cg_code) throws ClassNotFoundException, SQLException
+		{
+			int result = 0;
+			
+			Connection conn = null;
+			conn = DBConn.getConnection();
+			
+			String sql = "DELETE FROM BLOCKED_GROUP WHERE US_CODE = ? AND CG_CODE = ?";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(us_code));
+			pstmt.setString(2, cg_code);
+			
+			result = pstmt.executeUpdate();
+			
+			pstmt.close();
+			DBConn.close();
+			
+			return result; 
+			
+		}
 		
-		String sql = "UPDATE GROUP_INVITE SET GI_RESPONSE = SYSDATE, RS_CODE = 2 WHERE GI_CODE = ?";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, gi_code);
-		
-		result = pstmt.executeUpdate();
-		
-		pstmt.close();
-		DBConn.close();
-		
-		return result;
-	}
-	
-	// 그룹 초대거절 → 차단할래요 버튼 클릭시 그룹차단테이블 업데이트
-	public int block(int cg_code, int us_code) throws ClassNotFoundException, SQLException
-	{
-		int result = 0;
-		
-		Connection conn = null;
-		conn = DBConn.getConnection();
-		
-		String sql = "INSERT INTO BLOCKED_GROUP(BG_CODE, CG_CODE, US_CODE, BG_DATE) VALUES(SEQ_BLOCKED_GROUP.NEXTVAL, ?, ?, SYSDATE)";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, cg_code);
-		pstmt.setInt(2, us_code);
-		
-		result = pstmt.executeUpdate();
-		
-		pstmt.close();
-		DBConn.close();
-		
-		return result;
-	}
-	
 	
 }
