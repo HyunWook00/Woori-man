6c8ea8f497885efda1af3f884292e1b985d92797
김경태 <ktk6216@naver.com>
2024-03-25 22:46:28 +0900

    2024-03-25 비빌번호 찾기  8~20 자리 스크립트 설정

--- Woori-man/WebContent/js/pwdResetForm.js
+++ Woori-man/WebContent/js/pwdResetForm.js
@@ -11,32 +11,18 @@ $(document).ready(function()
 		var pwd1 = $("#us_pwd1").val();
 		var pwd2 = $("#us_pwd2").val();
 		
-		if (!$.trim($("#us_pwd1").val()) )
+		
+		if ($.trim($("#us_pwd1").val()).length < 8 || $.trim($("#us_pwd1").val()).length > 20 )
 		{
-			alert("비밀번호를 입력해주세요.");
+			alert("8 ~ 20 자리의 비밀번호를 입력해주세요.")
 			$("#us_pwd1").focus();
 			return;
 		}
-		if (!$.trim($("#us_pwd2").val()) )
-		{
-			alert("비밀번호 확인을 입력해주세요.");
-			$("#us_pwd2").focus();
-			return;
-		}
-		
-		
 		if (pwd1 != pwd2)
 		{
 			alert("비밀번호가 일치하지 않습니다.");
 			return;
 		}
-		
-		if (pwd1 == "" || pwd2 == "")
-		{
-			alert("올바른 비밀번호를 입력해주세요.");
-			return;
-		}
-		
 		$("#resetForm").submit();
 		alert("비밀번호 변경이 완료되었습니다.");
 		
